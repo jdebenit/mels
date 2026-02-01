7c645303c942264d370472a86f796d2e92ff2985
Jorge Francisco de Benito Montoya <jf.benito@gmail.com>
2026-02-01 18:48:22 +0100

    cambio dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jdebenit.github.io',
-  base: '/mels',
+  site: 'https://www.mutantesenlasombra.es',
+  base: '/',
   integrations: [react()]
 });
